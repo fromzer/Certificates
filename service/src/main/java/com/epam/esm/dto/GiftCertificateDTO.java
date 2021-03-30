36b13608275a8d57c6481275674c4a5ead399a96
Egor Mikheev <fromzer@mail.ru>
2021-03-30 20:01:00 +0300

    change DTOs

--- service/src/main/java/com/epam/esm/dto/GiftCertificateDTO.java
+++ service/src/main/java/com/epam/esm/dto/GiftCertificateDTO.java
@@ -1,15 +1,25 @@
 package com.epam.esm.dto;
 
-import com.epam.esm.entity.GiftCertificate;
-import com.epam.esm.entity.GiftTag;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import java.util.List;
+import java.math.BigDecimal;
+import java.time.ZonedDateTime;
+import java.util.Set;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class GiftCertificateDTO {
-    private GiftCertificate giftCertificate;
-    private List<GiftTag> giftTags;
+    private Long id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private Integer duration;
+    private ZonedDateTime createDate;
+    private ZonedDateTime lastUpdateDate;
+    private Set<GiftTagDTO> tags;
 }
--- service/src/main/java/com/epam/esm/dto/GiftTagDTO.java
+++ service/src/main/java/com/epam/esm/dto/GiftTagDTO.java
@@ -1,19 +1,15 @@
 package com.epam.esm.dto;
 
-import com.epam.esm.entity.GiftCertificate;
-import com.epam.esm.entity.GiftTag;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.util.List;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class GiftTagDTO {
-    private GiftTag giftTag;
-    private List<GiftCertificate> giftCertificates;
+    private Long id;
+    private String name;
 }
