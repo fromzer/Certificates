0ae1a0afc6f6649a18c3b990fe97c8c19f496cc3
Egor Mikheev <fromzer@mail.ru>
2021-03-30 19:59:09 +0300

    update entities

--- persistence/src/main/java/com/epam/esm/entity/Certificate.java
+++ persistence/src/main/java/com/epam/esm/entity/Certificate.java
@@ -9,13 +9,15 @@ import java.math.BigDecimal;
 import java.time.ZonedDateTime;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class Certificate implements Entity{
     private Long id;
     private String name;
     private String description;
     private BigDecimal price;
-    private int duration;
+    private Integer duration;
     private ZonedDateTime createDate;
     private ZonedDateTime lastUpdateDate;
 }
--- persistence/src/main/java/com/epam/esm/entity/Tag.java
+++ persistence/src/main/java/com/epam/esm/entity/Tag.java
@@ -1,9 +1,13 @@
 package com.epam.esm.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class Tag implements Entity {
     private Long id;
