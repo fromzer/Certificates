a63caf8a4aa349769e2addeab6a472ca55abbdad
Egor Mikheev <fromzer@mail.ru>
2021-04-07 20:22:40 +0300

    update exception handler

--- web/src/main/java/com/epam/esm/exception/RestExceptionHandler.java
+++ web/src/main/java/com/epam/esm/exception/RestExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -87,6 +88,17 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
                 .collect(Collectors.toList());
         body.put("errorMessage", errors);
         body.put("errorCode", status.value() + "" + (status.value() / 10));
+        return new ResponseEntity<>(body, headers, status);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        Locale locale = Locale.getDefault();
+        String msg = messageSource.getMessage(TYPE_MISMATCH_MESSAGE, null, locale);
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("status", status.value());
+        body.put("errorMessage", msg);
+        body.put("errorCode", status.value() + "" + (status.value() / 10 + 14));
         return new ResponseEntity<>(body, headers, status);
     }
 }
